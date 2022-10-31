5548976581487eefab6d5d586736b606256aa87b
luffykaiyuan <luffykaiyuan@163.com>
2022-10-31 16:18:14 +0800

    mybatis修改，redis基本接口

--- src/main/java/com/luffykaiyuan/shvideo/controller/Hello.java
+++ src/main/java/com/luffykaiyuan/shvideo/controller/Hello.java
@@ -4,6 +4,7 @@ import com.luffykaiyuan.shvideo.dao.VideoSourceMapper;
 import com.luffykaiyuan.shvideo.po.VideoSource;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,35 +22,37 @@ public class Hello {
 
     @GetMapping("/hello")
     public BufferedImage hello(){
-        try {
-            String path = "C:\\Users\\Luffykaiyuan\\Desktop\\com.bean.BusinessMov_40004.txt";
-            File file = new File(path);
-            InputStreamReader isr = new InputStreamReader(new FileInputStream(file),"utf-8");
-            BufferedReader br = new BufferedReader(isr);
-            String content = br.readLine() ;
-            br.close();
-            isr.close();
 
-            JSONObject jsonObj = JSONObject.fromObject(content);
-            VideoSource videoSource = new VideoSource();
-            videoSource.setId(jsonObj.getString("identCode"));
-            videoSource.setName(jsonObj.getString("name"));
-            videoSource.setContent(jsonObj.getString("content"));
-            videoSource.setCover(jsonObj.getString("cover"));
-            videoSource.setSecretCover(jsonObj.getString("secretCover"));
-            videoSource.setSecretCoverText(jsonObj.getString("secretCoverTxt"));
-            videoSource.setRatio(jsonObj.getString("ratio"));
-            videoSource.setSize(jsonObj.getInt("size"));
-            videoSource.setHttpContent(jsonObj.getString("httpContent"));
-            videoSource.setDuration(jsonObj.getInt("duration"));
-            videoSource.setServerIp(jsonObj.getString("serverIp"));
-            videoSource.setXvalue(jsonObj.getString("xvalue"));
-            videoSource.setXkey(jsonObj.getString("xkey"));
 
-            videoSourceMapper.insert(videoSource);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            String path = "C:\\Users\\Luffykaiyuan\\Desktop\\com.bean.BusinessMov_40004.txt";
+//            File file = new File(path);
+//            InputStreamReader isr = new InputStreamReader(new FileInputStream(file),"utf-8");
+//            BufferedReader br = new BufferedReader(isr);
+//            String content = br.readLine() ;
+//            br.close();
+//            isr.close();
+//
+//            JSONObject jsonObj = JSONObject.fromObject(content);
+//            VideoSource videoSource = new VideoSource();
+//            videoSource.setId(jsonObj.getString("identCode"));
+//            videoSource.setName(jsonObj.getString("name"));
+//            videoSource.setContent(jsonObj.getString("content"));
+//            videoSource.setCover(jsonObj.getString("cover"));
+//            videoSource.setSecretCover(jsonObj.getString("secretCover"));
+//            videoSource.setSecretCoverText(jsonObj.getString("secretCoverTxt"));
+//            videoSource.setRatio(jsonObj.getString("ratio"));
+//            videoSource.setSize(jsonObj.getInt("size"));
+//            videoSource.setHttpContent(jsonObj.getString("httpContent"));
+//            videoSource.setDuration(jsonObj.getInt("duration"));
+//            videoSource.setServerIp(jsonObj.getString("serverIp"));
+//            videoSource.setXvalue(jsonObj.getString("xvalue"));
+//            videoSource.setXkey(jsonObj.getString("xkey"));
+//
+//            videoSourceMapper.insert(videoSource);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         return null;
     }
 
